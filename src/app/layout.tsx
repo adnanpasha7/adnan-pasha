c540fdad45be8017516bf956ed57e2f0f14a318e
Adnan Pasha <92546855+adnanpasha7@users.noreply.github.com>
2026-01-25 19:47:06 +0000

    feat: initial integration & tailwind setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "./globals.css";
+import { Inter, DM_Serif_Display, Space_Mono } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const dmSerif = DM_Serif_Display({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-dm-serif",
+});
+
+const spaceMono = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-space-mono",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${dmSerif.variable} ${spaceMono.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
