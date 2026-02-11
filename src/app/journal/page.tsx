c79bb4a270c89956c72dc9947e192b82d4059cb5
Adnan Pasha <92546855+adnanpasha7@users.noreply.github.com>
2026-02-11 23:44:42 +0000

    Switch back to proper FTP deploy

--- src/app/journal/page.tsx
+++ src/app/journal/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import JournalClient from "./JournalClient";
 
+export const dynamic = "force-static";
+
 export default function JournalPage() {
   return (
     <Suspense fallback={<div />}>
